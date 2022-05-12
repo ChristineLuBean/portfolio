75d3fe843c07b8c062e7b7c42e66a60f5101c639
Christine LuBean <christinemlubean@gmail.com>
2022-05-11 17:14:09 -0700

    more responsiveness fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -156,8 +156,10 @@ $(function(){  // $(document).ready shorthand
   });
 
   function menuOnClick() {
+	
 	document.getElementById("menu-bar").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("clear-bg-toggle");
+	document.getElementById("nav").classList.toggle("nav-bg-toggle");
+	document.getElementById("navLinks").classList.toggle("noDisplay");
 	document.getElementById("menu2").classList.toggle("change");
 	document.getElementById("menu-bg").classList.toggle("change-bg");
   }
