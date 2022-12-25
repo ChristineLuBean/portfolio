ee929788cc3f6461a677114f2f105d2d12a5ad3d
Christine LuBean <christinemlubean@gmail.com>
2022-12-24 21:42:10 -0800

    added new parallax library

--- js/main.js
+++ js/main.js
@@ -1,38 +1,32 @@
-let lastScrollTop = 0
-// Get the navivation-bar element
-const navBar = document.getElementById('navigation-bar')
+let lastScrollTop = 0;
 
-window.onscroll = function () {
+// Get the navivation-bar element
+const navBar = document.getElementById('navigation-bar');
+
+// Get all sections with the 'fade-in' class
+const sections = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', function () {
   // Get the current scroll position
   const currentScrollTop =
-    window.pageYOffset || document.documentElement.scrollTop
+    window.pageYOffset || document.documentElement.scrollTop;
 
+  // Toggle the 'hidden' class on the navivation-bar based on the scroll direction
   if (currentScrollTop > lastScrollTop) {
-    // Scrolling down - hide the navivation-bar
-    navBar.classList.add('hidden')
+    navBar.classList.add('hidden');
   } else {
-    // Scrolling up - show the navigation-bar
-    navBar.classList.remove('hidden')
+    navBar.classList.remove('hidden');
   }
 
-  lastScrollTop = currentScrollTop
+  lastScrollTop = currentScrollTop;
 
-  // Get all sections with the 'fade-in' class
-  const sections = document.querySelectorAll('.fade-in')
+  // Add the 'visible' class to sections in the viewport
   for (section of sections) {
-    // Get the bounding rectangle for each section
-    const rect = section.getBoundingClientRect()
-    // Calculate the top and bottom positions of the section
-    const top = rect.top
-    const bottom = rect.bottom
+    const rect = section.getBoundingClientRect();
+    const top = rect.top;
+    const bottom = rect.bottom;
     if (top < window.innerHeight && bottom > 0) {
-      // The section is in the viewport - add the 'visible' class
-      section.classList.add('visible')
+      section.classList.add('visible');
     }
   }
-}
-
-function toggleMenu() {
-  // Toggle the 'navigation-bar-collapsed' class on the navigation-bar
-  navBar.classList.toggle('navigation-menu-collapsed')
-}
+});
